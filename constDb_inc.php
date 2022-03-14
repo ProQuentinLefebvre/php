4667bc06ec1043242181b379849b77eeab2b06ef
ProQuentinLefebvre <98103352+ProQuentinLefebvre@users.noreply.github.com>
2022-03-14 10:09:17 +0100

    Create constDb_inc.php

---
+++ constDb_inc.php
@@ -0,0 +1,11 @@
+<?php
+  // Database 
+  $dbservername = "localhost";
+  //$dbservername = "localhost:3306"; //HostnameOuIpAdress:PortDuServerDB
+  //ou 127.0.0.1  // HostnameOuAddresseIP:PortDuServerSQL  
+  //ex: localhost:3306 pour mySQL ou ex: localhost:1433 pour SqlServer 
+  $dbusername = "root";
+  $dbpassword = "root";
+  $dbname = "DB location";
+
+  $dbEncodng ="SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci";
