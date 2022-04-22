0e8bec601b2bd66c54346be7ec77e345a274af69
ProQuentinLefebvre <98103352+ProQuentinLefebvre@users.noreply.github.com>
2022-04-22 10:27:00 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,21 +1,29 @@
 <?php
-
+include("connect.php");
+session_start();
+$errorMessage = $user = $pass = "";
 // Validation du formulaire
-if (isset($_POST['email']) &&  isset($_POST['password'])) {
-    foreach ($users as $user) {
-        if (
-            $user['email'] === $_POST['email'] &&
-            $user['password'] === $_POST['password']
-        ) {
-            $loggedUser = [
-                'email' => $user['email'],
-            ];
+if (isset($_POST['email']) && isset($_POST['password'])) {
+    $user = $_POST['email'];
+    $pass = $_POST['password'];
+    try {
+        $query = "select * from Client where Email=:Email and Passwords =:Password";
+        $stmt = $db->prepare($query);
+        $stmt->bindParam(':Email', $user, PDO::PARAM_STR);
+        $stmt->bindValue(':Password', $pass, PDO::PARAM_STR);
+        $stmt->execute();
+        $count = $stmt->rowCount();
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        if ($count == 1) {
+            $_SESSION['sess_user_id'] = $row['Email'];
+            $_SESSION['sess_user_name'] = $row['Nom'];
+            header('location:home.php');
+            exit;
         } else {
-            $errorMessage = sprintf('Les informations envoyées ne permettent pas de vous identifier : (%s/%s)',
-                $_POST['email'],
-                $_POST['password']
-            );
+            $errorMessage = "Invalid username and password!";
         }
+    } catch (PDOException $e) {
+        die ("Error : " . $e->getMessage());
     }
 }
 ?>
@@ -23,14 +31,13 @@ if (isset($_POST['email']) &&  isset($_POST['password'])) {
 <!--
    Si utilisateur/trice est non identifié(e), on affiche le formulaire
 -->
-<?php if(!isset($loggedUser)): ?>
-<form action="home.php" method="post">
+<form action="login.php" method="post">
     <!-- si message d'erreur on l'affiche -->
-    <?php if(isset($errorMessage)) : ?>
+    <?php if (isset($errorMessage)): ?>
         <div class="alert alert-danger" role="alert">
-            <?php echo $errorMessage; ?>
+            <?= $errorMessage ?>
         </div>
-    <?php endif; ?>
+    <?php endif;?>
     <div class="mb-3">
         <label for="email" class="form-label">Email</label>
         <input type="email" class="form-control" id="email" name="email" aria-describedby="email-help" placeholder="you@exemple.com">
@@ -42,11 +49,29 @@ if (isset($_POST['email']) &&  isset($_POST['password'])) {
     </div>
     <button type="submit" class="btn btn-primary">Envoyer</button>
 </form>
-<!-- 
-    Si utilisateur/trice bien connectée on affiche un message de succès
--->
-<?php else: ?>
-    <div class="alert alert-success" role="alert">
-        Bonjour <?php echo $loggedUser['email']; ?> et bienvenue sur le site !
-    </div>
-<?php endif; ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" 
+        rel="stylesheet"
+    >
+</head>
+<body class="d-flex flex-column min-vh-100">
+    <div class="container">
+
+    <!-- Navigation -->
+   
+
+    <!-- Inclusion du formulaire de connexion -->
+    <?php include_once('login.php'); ?>
+        
+
+    
+</body>
+</html>
