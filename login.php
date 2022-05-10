281db5fc1d3fc8b5ef68e4334e3d6ac115d1adf4
ProQuentinLefebvre <98103352+ProQuentinLefebvre@users.noreply.github.com>
2022-05-10 11:50:58 +0200

    Update login.php

--- login.php
+++ login.php
@@ -17,7 +17,7 @@ if (isset($_POST['email']) && isset($_POST['password'])) {
         if ($count == 1) {
             $_SESSION['sess_user_id'] = $row['Email'];
             $_SESSION['sess_user_name'] = $row['Nom'];
-            header('location:home.php');
+            header('location:Client.php');
             exit;
         } else {
             $errorMessage = "Invalid username and password!";
@@ -48,6 +48,7 @@ if (isset($_POST['email']) && isset($_POST['password'])) {
         <input type="password" class="form-control" id="password" name="password">
     </div>
     <button type="submit" class="btn btn-primary">Envoyer</button>
+    <a href="formulaire.php?action=create">inscription</a>
 </form>
 
 <!DOCTYPE html>
